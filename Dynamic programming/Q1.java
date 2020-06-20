7c9ff7118443c9a7d1463b16012a8dfff787b9bf
Dinithi-Hasanika <46222840+Dinithi-Hasanika@users.noreply.github.com>
2020-06-20 20:06:22 +0530

    Add files via upload

---
+++ Dynamic programming/Q1.java
@@ -0,0 +1,45 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+class Q1 {
+    static int [][] cost = {{0, 3, 12, 23, 41}, // cost from 0 
+            {0, 0,  2,  4, 34}, // cost from 1
+            {0, 0,  0,  12, 3}, // cost from 2
+            {0, 0,  0,  0, 12}, // cost from 3
+            {0, 0,  0,  0,  0}  // cost from 4
+    };
+
+    static int iMax = 5;
+    static int jMax = 5;
+
+    // Just for testing, min cost from 0 to 4 should be 8.
+    static int answer = 8;
+
+    public static int minCost(int i, int j) {
+        // broken implementation
+        if (i == j || i+1 == j) {
+            return cost[i][j];
+        }
+
+        int minValue = cost[i][j];
+
+        for (int k = i+1; k<j; k++)
+        {
+            int check = minCost(i, k) +
+                    minCost( k, j);
+            if (check < minValue)
+                minValue = check;
+        }
+        return minValue;
+
+    }
+
+    public static void main(String [] args) {
+        int r = minCost(0,4);
+        if(r == answer)
+            System.out.println("Your implementation might be correct");
+        else
+            System.out.println("Too bad. Try again (" + r + ")");
+    }
+}
+    
